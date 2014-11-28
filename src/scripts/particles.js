051eb12ee5765dce8ef47e238f27de27ae741ecc
Kyle King <kmking72@gmail.com>
2014-11-28 00:50:09 -0500

    Darker particles

--- src/scripts/particles.js
+++ src/scripts/particles.js
@@ -1,6 +1,6 @@
 particlesJS('particles-js', {
     particles: {
-      color: '#fff',
+      color: '#2c3e50',
       shape: 'circle',
       opacity: 1,
       size: 2.5,
@@ -9,7 +9,7 @@ particlesJS('particles-js', {
       line_linked: {
         enable_auto: true,
         distance: 250,
-        color: '#fff',
+        color: '#2c3e50',
         opacity: 0.5,
         width: 1,
         condensed_mode: {
